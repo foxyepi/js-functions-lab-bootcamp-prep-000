44cc3574b30a3725d37e20effc4af0843ee9d7aa
Joseph Epifano <joepifano@gmail.com>
2017-12-23 19:08:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown (days, holiday) {
-  return `It\'s ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
 
 
